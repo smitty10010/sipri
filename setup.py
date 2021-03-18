f90c6a55fcabffc8159ac8bcc6900ab0f9e52ae3
Robert <35673753+smitty10010@users.noreply.github.com>
2021-03-17 21:42:03 -0600

    setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '1.0'
+PACKAGE_NAME = 'sipri'
+AUTHOR = 'Robert S.'
+AUTHOR_EMAIL = ''
+URL = 'https://github.com/smitty10010/sipri'
+
+LICENSE = 'MIT License'
+DESCRIPTION = "A simple way to access Stockholm International Peace Research Institute's (SIPRI) trade register API and return a CSV file."
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'requests'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
